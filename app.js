6eaa13ad879beee7ff0c62e2e02dcc2bc79b4609
Philip Hutchins <philip.hutchins@dealermatch.com>
2014-08-26 12:26:15 -0400

    adding some testing

--- app.js
+++ app.js
@@ -10,9 +10,31 @@ app.set('view engine', 'jade');
 // instruct express to serve up static assets
 app.use(express.static('public'));
 
+// Error handling
+app.use(function(err, req, res, next){
+  console.error(err.stack);
+  res.send(500, 'Something broke!');
+});
+
+// Test Object
+function Item(name, price) {
+  this.name = name;
+  this.price = price;
+}
+
+// Test Data
+var items = [
+  new Item('lamp', '247.00'),
+  new Item('desk', '22.00'),
+  new Item('book', '328')
+];
+
 // set routes
 app.get('/', function(req, res) {
-  res.render('index');
+  res.render('index', { items: items });
+});
+app.get('/hello.txt', function(req, res) {
+  res.send('Hello World');
 });
 
 // Set server port
